4e0aba97ccbbbb0e454a1c8b3ebf9843e546d4d7
kayfoh <kayfoh@gmail.com>
2022-12-16 20:12:58 -0600

    Added header to frontend file

--- finance_app/frontend.py
+++ finance_app/frontend.py
@@ -1,9 +1,11 @@
-from sidebar import sidebar
+import streamlit as st
 
+from sidebar import sidebar
 
 def frontend():
     sidebar()
 
 
 def main_frontend():
-    ...
+    st.write('WELCOME TO FINANCE APP')
+    
