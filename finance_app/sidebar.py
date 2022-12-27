20e4c5051952ca956f49d17acf6b96c76e9ea303
dswiredu <dswiredu@gmail.com>
2022-12-27 15:56:23 -0500

    Easily generate positions

--- finance_app/sidebar.py
+++ finance_app/sidebar.py
@@ -1,12 +1,19 @@
 import streamlit as st
 from utils import time_greeting
+from data_processing import get_advisor_client_list
 
 APP_TITLE = "Portfolio Management App"
 
 
-def sidebar(**options):
-    if "name" in options:
-        greeting = f"{time_greeting()} {options['name']}!"
-        st.sidebar.title(greeting)
+def sidebar(**opts):
+    greeting = f"{time_greeting()} {opts['name']}!"
+    st.sidebar.title(greeting)
     st.sidebar.title(APP_TITLE)
     st.sidebar.subheader("Manage your Portfolio")
+
+    clients = get_advisor_client_list(opts["username"])
+
+    selected_client = st.sidebar.selectbox(
+        'Manage your clients', clients, index=0, help="List of clients"
+    )
+    return selected_client
