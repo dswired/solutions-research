c1fd58e1714111b933a87cf78ba6c1d3d0c2ad51
dswired <david.wiredu@d1g1t.com>
2024-04-16 21:02:12 -0400

    fina-advisor-crm-start

---
+++ fin-advisor-crm/static/js/main.js
@@ -0,0 +1,13 @@
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+
+sidebarToggle.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+const navLinkEls = document.querySelectorAll('.sidebar-link');
+navLinkEls.forEach(navLinkEl => {
+    navLinkEl.addEventListener('click', () => {
+        document.querySelector('.activated')?.classList.remove('activated');
+        navLinkEl.classList.add('activated');
+    });
+});
