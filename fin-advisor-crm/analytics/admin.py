ffe0862c3d938dc6775ce6a1b882503c27ed3a26
dswired <david.wiredu@d1g1t.com>
2024-04-17 23:18:06 -0400

    added main and analytics apps

---
+++ fin-advisor-crm/analytics/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+class SecurityPriceAdmin(admin.ModelAdmin):
+    list_display = ["securityid", "date", "price"]
+    search_fields = ["securityid"]
+    ordering = ["securityid", "date"]
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = [
+        "account",
+        "security",
+        "trade_date",
+        "transaction_type",
+        "transaction_quantity",
+        "transaction_amount",
+        "transaction_id",
+    ]
+    search_fields = ["account", "securityid", "transaction_type", "transaction_id"]
+    ordering = ["account", "security", "trade_date"]
+
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ["account", "security", "date", "quantity", "market_value"]
+    search_fields = ["account", "security", "date"]
+    ordering = ["account", "security", "date"]
+
+
+admin.site.register(SecurityPrice, SecurityPriceAdmin)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Position, PositionAdmin)
--- fin-advisor-crm/main/admin.py
+++ fin-advisor-crm/main/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from .models import *
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = [
+        "clientid",
+        "name",
+        "client_type",
+        "is_active",
+        "date_opened",
+        "advisor",
+    ]
+    search_fields = ["clientid", "advisor"]
+    ordering = ["clientid"]
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = [
+        "accountid",
+        "account_name",
+        "clientid",
+        "date_opened",
+        "is_active",
+    ]
+    search_fields = ["accountid", "clientid"]
+    ordering = ["accountid", "clientid"]
+
+
+class SecurityIndustryAdmin(admin.ModelAdmin):
+    search_fields = ["security_industry"]
+    ordering = ["security_industry"]
+
+
+class SecuritySectorAdmin(admin.ModelAdmin):
+    search_fields = ["security_sector"]
+    ordering = ["security_sector"]
+
+
+class SecurityAdmin(admin.ModelAdmin):
+    list_display = ["securityid", "name", "sector", "industry"]
+    search_fields = ["securityid"]
+    ordering = ["securityid"]
+
 
 # Register your models here.
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Account, AccountAdmin)
+admin.site.register(SecuritySector, SecuritySectorAdmin)
+admin.site.register(SecurityIndustry, SecurityIndustryAdmin)
+admin.site.register(Security, SecurityAdmin)
