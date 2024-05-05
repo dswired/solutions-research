e3f252997443bb847b4337f652ae026910e8b054
dswired <david.wiredu@d1g1t.com>
2024-05-05 12:53:20 -0400

    search by advisro-username

--- fin-advisor-crm/core/loaders/client.py
+++ fin-advisor-crm/core/loaders/client.py
@@ -9,9 +9,9 @@ def client(row: dict) -> None:
     Client.objects.get_or_create(
         advisor=advisor,
         clientid=row["clientid"],
-        name=row['name'],
-        date_opened=row['date_opened'],
-        inception_date=to_nullable_date(row['inception_date']),
-        client_type=row['client_type'].upper(),
-        is_active=to_bool(row['is_active'])
-        )
+        name=row["name"],
+        date_opened=row["date_opened"],
+        inception_date=to_nullable_date(row["inception_date"]),
+        client_type=row["client_type"].upper(),
+        is_active=to_bool(row["is_active"]),
+    )
--- fin-advisor-crm/core/utils.py
+++ fin-advisor-crm/core/utils.py
@@ -1,5 +1,6 @@
 def to_nullable_date(date: str) -> str:
     return date if date else None
 
+
 def to_bool(val: str) -> bool:
     return True if val == "TRUE" else False
