0e38471a7370a87c4376eacbb008988732b2bba5
kayfoh <kayfoh@gmail.com>
2024-05-05 16:14:56 -0500

    Restructured models.py file
    - Data Models broken down into Equity and Fixed Income
    - Equity broken down into public and private equities
    - Fixed Income broken down into short, medium, & long-term
    - Removed ticker as foreign key in equity trade's data

--- fin-advisor-crm/markets/models.py
+++ fin-advisor-crm/markets/models.py
@@ -1,10 +1,87 @@
 from django.db import models
 import constants as cons
 
-# Create your models here.
+
+
+##########################################################################################
+#################                       Equity Data                     ##################
+##########################################################################################
+
+class DailyPublicEquityTrades(models.Model):
+    """
+    Daily trading data for public equity securities trading on the ghana stock exchange. 
+    Data obtained from the Ghana Stock Exchange website: 
+    https://gse.com.gh/trading-and-data/#dailyshares
+    """
+
+    # Fields
+    trade_date = models.DateField()
+    ticker = models.CharField(
+        max_length=cons.LARGE_CHARACTER_MAX_LENGTH 
+    )
+    year_high = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    year_low = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    previous_closing_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    opening_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    last_transaction_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    closing_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    price_change = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    closing_bid_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    closing_offer_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    total_shares_traded = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    total_value_traded = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+
+    class Meta:
+        ordering = ["trade_date", "ticker"]
+        unique_together = ["trade_date", "ticker"]
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}, date: {self.trade_date}, ticker: {self.ticker}"
+
+
+
+
+class DailyPrivateEquityTrades(models.Model):
+    ...
+
+
+
 class PublicCompanies(models.Model):
     """
-    Information on listed companies.
+    Information on listed companies. Manually generated
     """
 
     # Fields
@@ -31,72 +108,145 @@ class PublicCompanies(models.Model):
 
 
 
-class DailyTrades(models.Model):
+
+##########################################################################################
+#################                   Fixed Income Data                   ##################
+##########################################################################################
+class DailyBondNoteTrades(models.Model):
     """
-    Raw daily trading data for all stocks on the GSE. 
+    Daily trading data for long-term fixed income securities - Government & Corporate 
+    Bonds, & Government Notes on the Ghana Fixed Income Market. Data obtained from 
+    Ghana Fixed Income Markets: https://gfim.com.gh/daily-trading-reports/.
     """
-
-    # Fields
-    date = models.DateField()
-    ticker = models.ForeignKey(
-        PublicCompanies, 
-        on_delete=models.PROTECT
+    trade_date = models.DateField()
+    tenor = models.CharField(
+        max_length=cons.CHARACTER_MAX_LENGTH,
     )
-    price = models.DecimalField(
-        max_digits=cons.MAX_DIGITS,
-        decimal_places=cons.DECIMAL_PLACES
-    )
-    change = models.DecimalField(
-        max_digits=cons.MAX_DIGITS,
-        decimal_places=cons.DECIMAL_PLACES
-    )
-    volume = models.BigIntegerField()
-
-    class Meta:
-        ordering = ["date", "ticker"]
-        unique_together = ["date", "ticker"]
-
-    def __str__(self) -> str:
-        return f"{super().__str__()}, name: {self.date}, ticker: {self.ticker}"
-
-
-
-class EquityTrades(models.Model):
-    """
-    Table for persisting data to web application.
-    """
-
-    # Fields
-    date = models.DateField()
-    ticker = models.ForeignKey(
-        PublicCompanies, 
-        on_delete=models.PROTECT
-    )
-    price = models.DecimalField(
-        max_digits=cons.MAX_DIGITS,
-        decimal_places=cons.DECIMAL_PLACES
-    )
-    change = models.DecimalField(
-        max_digits=cons.MAX_DIGITS,
-        decimal_places=cons.DECIMAL_PLACES
-    )
-    volume = models.BigIntegerField()
-    name = models.CharField(
+    security_type = models.CharField(
+        max_length=cons.CHARACTER_MAX_LENGTH
+    )     
+    security_description = models.CharField(
         max_length=cons.LARGE_CHARACTER_MAX_LENGTH
+    )   
+    isin = models.CharField(
+        max_length=cons.CHARACTER_MAX_LENGTH
     )
-    industry = models.CharField(
-        max_length=cons.LARGE_CHARACTER_MAX_LENGTH
+    opening_yield = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    closing_yield = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    opening_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    closing_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    volume_traded = models.BigIntegerField()
+    mumber_traded = models.BigIntegerField()
+    day_high_yield = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    day_low_yeild = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    day_high_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=True
+    )
+    day_low_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
     )    
-    sector = models.CharField(
-        max_length=cons.LARGE_CHARACTER_MAX_LENGTH
-    )
+    days_to_maturity = models.BigIntegerField()
+    maturity_date = models.DateField()
 
     class Meta:
-        ordering = ["date", "ticker"]
-        unique_together = ["date", "ticker"]
-    
-    def __str__(self) -> str:
-        return f"{super().__str__()}, date: {self.date}, ticker: {self.ticker}"
+        ordering = ["trade_date", "isin"]
+        unique_together = ["trade_date", "isin"]
 
+    def __str__(self) -> str:
+        return f"{super().__str__()}, name: {self.trade_date}, ticker: {self.isin}"
+
+
+
+class DailyBillCODPaperTrades(models.Model):
+    """
+    Daily trading data for medium-term fixed income securities - treasury bills, 
+    certificates of deposit, commercial papers. Data obtained from Ghana Fixed Income
+    Markets: https://gfim.com.gh/daily-trading-reports/
+    """
+    trade_date = models.DateField()
+    tenor = models.CharField(
+        max_length=cons.CHARACTER_MAX_LENGTH,
+    )
+    security_type = models.CharField(
+        max_length=cons.CHARACTER_MAX_LENGTH
+    )     
+    security_description = models.CharField(
+        max_length=cons.LARGE_CHARACTER_MAX_LENGTH
+    )   
+    isin = models.CharField(
+        max_length=cons.CHARACTER_MAX_LENGTH
+    )
+    opening_yield = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    closing_yield = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    opening_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    closing_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    volume_traded = models.BigIntegerField()
+    mumber_traded = models.BigIntegerField()
+    day_high_yield = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    day_low_yeild = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )
+    day_high_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=True
+    )
+    day_low_price = models.DecimalField(
+        max_digits=cons.MAX_DIGITS,
+        decimal_places=cons.DECIMAL_PLACES
+    )    
+    days_to_maturity = models.BigIntegerField()
+    maturity_date = models.DateField()
+
+    class Meta:
+        ordering = ["trade_date", "isin"]
+        unique_together = ["trade_date", "isin"]
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}, name: {self.trade_date}, ticker: {self.isin}"
+
+
+
+class DailyRepoTrades(models.Model):
+    """
+    Daily trading data for short-term fixed income securities - Repos. Data obtained 
+    from Ghana Fixed Income Markets: https://gfim.com.gh/daily-trading-reports/
+    """
+    ...
 
 
