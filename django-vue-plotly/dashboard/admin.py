23d4603bf64c20513308f5e74b437d6e0528c68c
dswired <david.wiredu@d1g1t.com>
2025-03-12 23:07:38 -0400

    tickers-added-vue

---
+++ django-vue-plotly/dashboard/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Ticker, TickerMetadata, HistoricalPrice
+
+@admin.register(Ticker)
+class TickerAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'name', 'exchange')
+    search_fields = ('symbol', 'name')
+    list_filter = ('sector', 'industry')
+
+
+@admin.register(TickerMetadata)
+class TickerMetadataAdmin(admin.ModelAdmin):
+    list_display = ('ticker', 'last_updated', 'market_cap', 'beta')
+
+
+@admin.register(HistoricalPrice)
+class HistoricalPriceAdmin(admin.ModelAdmin):
+    list_display = ('ticker', 'date', 'close_price', 'volume')
+    list_filter = ('ticker', 'date')
+    search_fields = ('ticker__symbol',)
